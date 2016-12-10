28c3eca6b3a4481d3827b091d6e06964af31bbf5
russvetsper <russvetsper@gmail.com>
2016-12-09 22:13:15 -0800

    could not apply ajax. 8 hours are up

--- src/MrFixIt/Models/Job.cs
+++ src/MrFixIt/Models/Job.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace MrFixIt.Models
 {
     //builds out table for jobs
     //incorrect. our database should be plural 
+    [Table("Jobs")]
     public class Job
     {
         [Key]
--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,1 +1,18 @@
 ﻿var test = "it works";
+
+
+
+    $(document).ready(function () {
+        $('.Claim This Job').click(function () {
+            $.ajax({
+                type: 'GET',
+                dataType: 'json',
+                contentType: 'application/json',
+                url: '/Claim/Job'
+                success: function (model) {
+                    var resultString = '<br>Title: ' + model.title + '<br>Description: ' + model.description;
+                    $('#result1').html(resultString);
+                }
+            });
+        });
+    
